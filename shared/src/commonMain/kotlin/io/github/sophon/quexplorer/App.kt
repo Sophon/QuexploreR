f85c3fb8b31d63219f85cedc098fb700a93536ec
Le Duy Anh <lenduya@gmail.com>
2026-06-13 17:53:46 +0200

    feat (navigation): bottom bar navigates screens

--- shared/src/commonMain/kotlin/io/github/sophon/quexplorer/App.kt
+++ shared/src/commonMain/kotlin/io/github/sophon/quexplorer/App.kt
@@ -7,21 +7,30 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
+import androidx.navigation3.ui.NavDisplay
 import androidx.savedstate.serialization.SavedStateConfiguration
 import io.github.sophon.quexplorer.core.ui.BottomBar
+import io.github.sophon.quexplorer.core.ui.PlaceholderScreen
 import io.github.sophon.quexplorer.navigation.Destination
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
+import kotlin.collections.listOf
 
 private val navConfig = SavedStateConfiguration {
     serializersModule = SerializersModule {
@@ -49,14 +58,17 @@ fun App() {
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.surface),
             ) {
-                //App nav display
+                AppNavDisplay(backStack = backStack)
 
                 BottomBar(
                     current = (backStack.first() as Destination),
                     onTabClick = { destination ->
                         backStack.clear()
                         backStack.add(destination)
-                    }
+                    },
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .navigationBarsPadding()
                 )
             }
         }
@@ -68,5 +80,24 @@ private fun AppNavDisplay(
     backStack: NavBackStack<NavKey>,
     modifier: Modifier = Modifier
 ) {
-    //TODO:
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryDecorators = listOf(
+            rememberSaveableStateHolderNavEntryDecorator(),
+            rememberViewModelStoreNavEntryDecorator(),
+        ),
+        modifier = modifier
+            .fillMaxSize()
+            .statusBarsPadding(),
+        entryProvider = entryProvider {
+            entry<Destination.Catalog> {
+                PlaceholderScreen(label = "Catalog")
+            }
+
+            entry<Destination.Scanner> {
+                PlaceholderScreen(label = "Scanner")
+            }
+        }
+    )
 }
