61b274de240397faa2d8a8c738ee19da310c2549
Le Duy Anh <lenduya@gmail.com>
2026-06-13 17:44:15 +0200

    feat (navigation): destinations

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -43,6 +43,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(libs.composePreviewMultiplatform)
+            implementation(compose.materialIconsExtended)
 
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlin.date.time)
